96d861cbda9d740f2c742bf016b8e79690c5ca8c
Joden He <824923282@qq.com>
2021-04-08 23:16:49 +0800

    add spring gateway demo

---
+++ eureka-gateway-client/src/main/java/top/joden123/eurekagatewayclient/EurekaGatewayClientApplication.java
@@ -0,0 +1,15 @@
+package top.joden123.eurekagatewayclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaGatewayClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaGatewayClientApplication.class, args);
+    }
+
+}
---
+++ eureka-gateway-client/src/test/java/top/joden123/eurekagatewayclient/EurekaGatewayClientApplicationTests.java
@@ -0,0 +1,13 @@
+package top.joden123.eurekagatewayclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGatewayClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
